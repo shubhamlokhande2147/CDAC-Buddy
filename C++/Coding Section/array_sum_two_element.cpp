d7acd6c2f00e27f35feddc942f17853261b0beb1
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-10 23:41:04 +0530

    Create DBT folder

---
+++ C++/Coding Section/array_sum_two_element.cpp
@@ -0,0 +1,28 @@
+/*one array + compare one element to each other and 
+if sum of two element is 6 then prints element
+
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{	int i,j,sum=0;
+
+	int arr[4]={1,5,7,1};
+	for(i=0;i<4;i++)
+	{
+	    for(j=i+1;j<5;j++)
+	    {
+	      sum=arr[i]+arr[j];
+	      if(sum==6)
+           {
+             cout<<"i : "<<arr[i]<<" "<<"j :"<<arr[j]<<endl;
+           }  
+	      
+        }	 		
+
+    }
+
+	
+	return 0;
+}
