878f53b62b1f139b3cacf671adb01e870094e86d
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-17 23:32:21 +0530

    up

---
+++ DBT/Data/procedure/procedure1_6.sql
@@ -0,0 +1,28 @@
+
+DROP PROCEDURE if EXISTS pro1 ;
+delimiter $
+CREATE PROCEDURE pro1(name varchar(200))
+BEGIN
+        DECLARE a INT;
+        set a=0;
+        DECLARE countdigit int;
+        set countdigit=0;
+        set @x=" ";
+        set @y=" ";
+
+        lb:LOOP
+           set a =a+1;
+           set @x = substring(name,a,1);
+
+               if(@x BETWEEN 'a' AND 'z') THEN 
+                   set @y=concat(@y,@x);
+                   SELECT @y;
+                   
+                    if a > LENGTH(name)-1 THEN
+                    leave lb;
+                    end if;
+               end if;                 
+        end LOOP lb;
+
+END $
+delimiter ;        
--- DBT/Data/procedure/procedure2.sql
+++ DBT/Data/procedure/procedure2.sql
@@ -1,12 +1,14 @@
 
 DROP PROCEDURE if EXISTS checkUser ;
 delimiter $
-CREATE PROCEDURE checkUser(_emailid varchar (50))
+CREATE PROCEDURE checkUser(p varchar (50))
 BEGIN
      DECLARE flag bool;
-             SELECT true into flag from login where emailid=_emailid;
+             SELECT true into flag from login where emailid=p;
              if flag THEN
-               SELECT * from login where emailid=_emailid ;
+               SET @x:=concat("SELECT * from login where emailid=p") ;
+               prepare z from @x;
+               execute z;
             else
                SELECT "ERROR";
              end if;       
