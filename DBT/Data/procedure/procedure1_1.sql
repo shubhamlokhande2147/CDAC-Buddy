0cc1f245139d0e86ca00e8ec72a3cf396312fe6d
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-18 23:48:55 +0530

    last update

--- DBT/Data/procedure/procedure1_1.sql
+++ DBT/Data/procedure/procedure1_1.sql
@@ -3,14 +3,24 @@ DROP PROCEDURE if EXISTS pro1 ;
 delimiter $
 CREATE PROCEDURE pro1(str varchar(50))
 BEGIN
--- set @x =" ";
- declare y int;
-set y:=0;
---            -- lb1:LOOP
-             --set y:=y+1;
-                select left(str,y);
+
+   declare x int ;
+   declare y int;
+
+   set x:=0;
+   set y:=0;
+   set y:=length(str);
+
+             lb1:LOOP
+             set x:=x+1;
                 
-            --end LOOP lb1;
-       
+               if x <= y THEN 
+                  SELECT SUBSTRING(str, x, 1) as string;
+             else            
+               leave lb1;
+            
+             end if;
+               end LOOP lb1; 
+              
 END $
 delimiter ;        
