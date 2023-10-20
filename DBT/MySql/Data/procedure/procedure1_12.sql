59be491a6b363b1ea129cb3a35a762a1576ee638
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-20 16:41:19 +0530

    push

--- DBT/MySql/Data/procedure/procedure1_12.sql
+++ DBT/MySql/Data/procedure/procedure1_12.sql
@@ -1,9 +1,9 @@
-drop procedure if exists que11;
+drop procedure if exists que12;
 delimiter $
-create procedure que11()
+create procedure que12(cid int)
 BEGIN
  
+      select c.cname,o.* from customers c join orders o on c.cnum=o.cnum where c.cnum=cid;
   
-
 end $
 delimiter ;
