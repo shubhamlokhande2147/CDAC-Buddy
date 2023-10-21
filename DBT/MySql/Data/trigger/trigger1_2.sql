7cf3c26564b0d894cd18ee9501ac00e7c25af590
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-21 20:07:26 +0530

    pushh

--- DBT/MySql/Data/trigger/trigger1_2.sql
+++ DBT/MySql/Data/trigger/trigger1_2.sql
@@ -2,13 +2,13 @@
 
 drop trigger if exists presc;
 delimiter $
-create trigger presc before insert on patient_pres for each row
+create trigger presc before insert on patient_pre for each row
 BEGIN
 	DECLARE flag BOOLEAN;
 	set flag=true;
 	select FALSE into flag FROM patient_reg where  pid=new.pid;
 	if flag THEN
-	 	signal sqlstate '45000' set message_text = "Person is not a registered  patient";
+	 	signal sqlstate '45000' set message_text = "Person is not a registered patient";
 	end if;
 
 END $
