c4ccfc7a0b33c0b49a7e73af8b41bd99bcfd2cdd
Harshvardhan Wadavane <71274010+harshwa007@users.noreply.github.com>
2023-10-22 21:24:30 +0530

    Update function1.sql

--- DBT/MySql/Data/function/function1.sql
+++ DBT/MySql/Data/function/function1.sql
@@ -14,3 +14,28 @@ begin
  
 end $
 delimiter ;
+/*
+drop function if exists que1;
+delimiter $
+create function que1(_deptno int) returns int
+deterministic
+BEGIN
+    declare sum1 int;
+    declare count1 int;
+    declare temp int;
+    set sum1:=0;
+    select count(*) into count1 from emp where deptno=_deptno group by deptno;
+    l1:loop
+        set count1=count1-1;
+        if count1>=0 then
+            select sal into temp from emp where deptno=_deptno limit 1,1;
+            set sum1=sum1+temp;
+
+        else 
+            leave l1;
+        end if;
+    end loop l1;
+    return sum1;
+END $
+delimiter ;
+*/
