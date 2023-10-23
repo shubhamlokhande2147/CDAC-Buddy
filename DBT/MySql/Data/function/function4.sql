8433524ffa00699431ea742d47144e422b09f1c2
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-10-23 08:36:18 +0530

    push

--- DBT/MySql/Data/function/function4.sql
+++ DBT/MySql/Data/function/function4.sql
@@ -1,12 +1,14 @@
 /*  use campus db  */
 
-drop FUNCTION if exists calculate_sum;
+drop FUNCTION if exists calculate_sum ;
 delimiter $
 create FUNCTION calculate_sum(_sid int) returns int
 deterministic
 BEGIN
-set @msum=0;
-	SELECT sum(marks) into @msum from student s join student_qualifications sq on s.id=sq.studentid where s.id=_sid;
-	return  @msum;
+declare msum int;
+set msum:=0;
+	SELECT sum(marks) into msum from student s join student_qualifications sq on s.id=sq.studentid where s.id=_sid;
+	
+	return  msum;
 end $
 delimiter ;
