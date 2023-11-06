a2c85c9e662988c5cb04e4645125fb943c37ba1a
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-11-06 22:19:52 +0530

    updates

---
+++ OOP's using JAVA/GarbageCollector/src/GarbageCollector.java
@@ -0,0 +1,23 @@
+
+public class GarbageCollector {
+
+
+public static class TestGarbage1{
+
+ public void finalize()
+ {
+	 System.out.println("object is garbage collected");
+ }
+
+ public static void main(String args[])
+ { 
+  TestGarbage1 s1=new TestGarbage1();
+  TestGarbage1 s2=new TestGarbage1();
+  s1=null;
+  s2=null;
+  System.gc();
+ }
+}
+
+
+}
