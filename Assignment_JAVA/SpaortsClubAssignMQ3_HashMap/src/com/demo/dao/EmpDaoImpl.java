31b9af86ad87b849367da1dedcf508b126be595f
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-11-11 08:43:55 +0530

    updates

--- Assignment_JAVA/SpaortsClubAssignMQ3_HashMap/src/com/demo/dao/EmpDaoImpl.java
+++ Assignment_JAVA/SpaortsClubAssignMQ3_HashMap/src/com/demo/dao/EmpDaoImpl.java
@@ -148,23 +148,24 @@ public class EmpDaoImpl implements EmpDao {
 
 	@Override
 	public List<Employee> sortByName() {
-		// TODO Auto-generated method stub
-		Comparator<Employee> name = (o1, o2) -> {
-			String n1, n2;
-			if (o1 instanceof Person) {
-				n1 = o1.getName();
-			}
-			if (o2 instanceof Person) {
-				n2 = o2.getName();
-			}
-			return n1.compareTo(n2);
-		};
-		List<Employee> rr = new ArrayList<>();
-		for (Employee e : hm.values()) {
-			rr.add(e);
-		}
-		rr.sort(null);
-		return rr;
+//		// TODO Auto-generated method stub
+//		Comparator<Employee> name = (o1, o2) -> {
+//			String n1, n2;
+//			if (o1 instanceof Person) {
+//				n1 = o1.getName();
+//			}
+//			if (o2 instanceof Person) {
+//				n2 = o2.getName();
+//			}
+//			return n1.compareTo(n2);
+//		};
+//		List<Employee> rr = new ArrayList<>();
+//		for (Employee e : hm.values()) {
+//			rr.add(e);
+//		}
+//		rr.sort(null);
+//		return rr;
+		return null;
 	}
 
 	// @Override
