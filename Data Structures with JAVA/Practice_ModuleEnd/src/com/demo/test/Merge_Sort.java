066123841a3f6c7d60a14c3a1a0da35d1684cc1a
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-03 09:49:35 +0530

    .

--- Data Structures with JAVA/Practice_ModuleEnd/src/com/demo/test/Merge_Sort.java
+++ Data Structures with JAVA/Practice_ModuleEnd/src/com/demo/test/Merge_Sort.java
@@ -2,10 +2,11 @@ package com.demo.test;
 
 import java.util.Scanner;
 
-public class Selection_Sort {
+public class Merge_Sort {
 
 	public static void main(String[] args) {
 		
+
    		Scanner sc = new Scanner(System.in);
    		
    		System.out.println("enter array size : ");
@@ -28,33 +29,20 @@ public class Selection_Sort {
            }
            
            
-           // selection sort
+           // merge sort
            
-           for(int i=0; i < arr.length-1; i++)
-           {
-    		   int min = i;
-
-        	   for(int j= i +1 ; j < arr.length; j++)
-        	   {
-        		   
-        		   if(arr[j] < arr[min])
-        		   {
-        			   min = j;
-        		   }
-        	   }
-        	   
-        	   //put smallest no. to i^th location
-        	   int temp = arr[i];
-			   arr[i] = arr[min];
-			   arr[min] = temp;
-        	   
-           }
            
-          	System.out.print("\nAfter Array : ");
-            for(int i=0; i < arr.length ; i++)
-            {
-        		System.out.print(arr[i] + " ");
-            }
+              mergesort(arr,0,arr.length - 1); //pass here array,first and last index
+           
+           
+           
+       	System.out.print("\nAfter Array : ");
+        for(int i=0; i < arr.length ; i++)
+        {
+    		System.out.print(arr[i] + " ");
+        }
+	
+           
 		
 	}
 	
