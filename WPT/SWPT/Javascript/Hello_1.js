6627636c5927bb4fe7e1e17fd6aeb36b2d0f13b9
ShubhamLokhande <75616296+shubhamlokhande2147@users.noreply.github.com>
2023-12-08 23:54:29 -0800

    Add files via upload

---
+++ WPT/SWPT/Javascript/Hello_1.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+
+let x;
+x= `shubham`
+console.log(x)
+console.log(typeof(x))
+
+x = new String("shubhamLOKHANDE")
+console.log(x)
+console.log(typeof(x))
+
+x=true;
+console.log(typeof(x))
+
+x=[1,2,35,46,56,"sdsd"]
+console.log("x : ",x)
+console.log(typeof(x))
+
