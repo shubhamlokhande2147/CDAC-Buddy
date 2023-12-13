7ff3bdaa63c08116879f868458bc5c3395c7b1ac
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-13 23:39:11 +0530

    .

---
+++ WPT/MyWebServer/Server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+let app = express()
+app.get("/",(req,res)=>{
+    console.log("request arrived")
+    res.send("Hi from Server...")
+})
+
+app.listen(7000,()=>{
+    console.log("Server running on port 7000....")
+})
