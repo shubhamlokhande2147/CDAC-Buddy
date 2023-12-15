58fa3aa622e308eddc2886e55bd45ce3b3475136
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-15 16:09:47 +0530

    save

--- WPT/MyWebServer/Server.js
+++ WPT/MyWebServer/Server.js
@@ -1,11 +1,26 @@
 import express from "express"
-
 let app = express()
-app.get("/hi",(req,res)=>{
+app.get('/',(req,res)=>{
     console.log("request arrived")
-    res.send("Hi from Server...")
+    res.send("Hi from server")
+
+})
+
+app.get("/getmsg",(req,res)=>{
+    res.send("If you dream it You can Achieve it!")
+})
+
+app.post("/postmsg",(req,res)=>{
+    res.send("Post it correctly")
+})
+
+app.put("/putmsg",(req,res)=>{
+    res.send("put it away")
+})
+app.delete("/deletemsg",(req,res)=>{
+    res.send("Delete it right now")
 })
 
 app.listen(7000,()=>{
-    console.log("Server running on port 7000....")
+    console.log("server is running on port 7000")
 })
