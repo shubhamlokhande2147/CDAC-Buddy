6cadd609638ff77716e56797d9b2b88c231104c5
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-19 23:41:16 +0530

    save

--- .NET/Day1/HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ .NET/Day1/HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a75c60c615448f6a45535835ab3dce439f0ec93")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
