00244530ec74d75f981947cc8fffdfeaa8dfd466
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-22 18:32:21 +0530

    save

--- .NET/Day4/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ .NET/Day4/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72790ea737b0e29f388108774aa6a27cd784d2cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
