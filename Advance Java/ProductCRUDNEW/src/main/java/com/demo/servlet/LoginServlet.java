5bfd394851ccd59a29fc64fd3f94f0729febf14d
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-22 23:49:16 +0530

    .

---
+++ Advance Java/ProductCRUDNEW/src/main/java/com/demo/servlet/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import com.demo.beans.MyUser;
+import com.demo.service.LoginService;
+import com.demo.service.LoginServiceImpl;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/validate")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		LoginService ls=new LoginServiceImpl();
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		//it will find, if user exists, then it will return MyUser Object, otherwise null
+		MyUser user=ls.validateUser(uname,pass);
+		System.out.println(user);
+		if(user!=null) {
+			//create a new session
+			HttpSession session=request.getSession();
+			//if(session.isNew()) {
+			session.setAttribute("user", user);
+			//}
+			//if th erole of the use ris admin 
+			if(user.getRole().equals("admin")) {
+				RequestDispatcher rd=request.getRequestDispatcher("displayproduct");
+				rd.forward(request, response);
+			}else {
+				out.println("you are a user");
+			}
+		}
+		else {
+			out.println("Wrong credentials, pls renter");
+			RequestDispatcher rd=request.getRequestDispatcher("Login.html");
+			rd.include(request,response);
+		}
+		
+	}
+
+}
---
+++ Advance Java/ProductCRUDNEW/src/main/java/com/demo/servlet/LogoutServlet.java
@@ -0,0 +1,31 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		session.invalidate();
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("Thank you for visiting.. do visit again....");
+	}
+
+	
+}
