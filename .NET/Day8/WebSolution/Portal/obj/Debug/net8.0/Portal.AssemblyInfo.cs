607f040c49503d8f07347f81d71ef548d3c54832
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-29 20:03:16 +0530

    mvc with classlib

--- .NET/Day8/WebSolution/Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ .NET/Day8/WebSolution/Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbbfd538f3e227be36fa6e1c18b4bc53022fa31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae548ae9c51845b04dfc07ff5cd91e1e4f1cb68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
