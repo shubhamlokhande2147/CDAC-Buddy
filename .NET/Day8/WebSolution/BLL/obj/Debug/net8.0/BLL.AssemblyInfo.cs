b100b148429171b3e3f144dcf14143dc13777e41
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2023-12-29 19:41:50 +0530

    MVC

--- .NET/Day8/WebSolution/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ .NET/Day8/WebSolution/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4096672f6e70c1c780dd8f191cca4072a1ca69e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbbfd538f3e227be36fa6e1c18b4bc53022fa31")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
