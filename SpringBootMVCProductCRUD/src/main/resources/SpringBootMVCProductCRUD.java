77e3a6adcbb1bf9630b276a9906bd4a021eed994
ShubhamLokhande <75616296+shubhamlokhande2147@users.noreply.github.com>
2024-01-17 11:54:34 +0530

    SpringBoot MVC CRUD

---
+++ SpringBootMVCProductCRUD/src/main/resources/SpringBootMVCProductCRUD.java
@@ -0,0 +1,14 @@
+
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootMVCProductCRUD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootMVCProductCRUD.class,args);
+	}
+
+}
